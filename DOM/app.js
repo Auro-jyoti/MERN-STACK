51d5968ea248b763d33adb2f4934cfcff7624973
Aurojyoti <developeratbackend@gmail.com>
2023-08-17 23:21:13 +0530

    unit converter, splitBill, add users

--- DOM/app.js
+++ DOM/app.js
@@ -1,7 +1,7 @@
 function joined() {
-   let title =  document.getElementById("title");
-   title.innerText = "Congratulations!!!";
-   
+    let title = document.getElementById("title");
+    title.innerText = "Congratulations!!!";
+
 }
 
 function convert() {
@@ -10,8 +10,37 @@ function convert() {
     let cm = m.value * 100;
 
     // console.log(cm);
-    let finalResult = m.value+ "m" + " is " + cm + "cm";
+    let finalResult = m.value + "m" + " is " + cm + "cm";
     let result = document.getElementById("result");
 
     result.innerText = finalResult;
+
+    m.value = " ";
+}
+
+function splitBill() {
+    let amount = document.getElementById('amount');
+    let persons = document.getElementById('persons');
+
+    let bill = (amount.value / persons.value).toFixed(2);
+
+    if (isNaN(bill)) {
+        document.getElementById("result-3").innerText = "Enter a valid Number";
+    } else {
+        document.getElementById("result-3").innerText = bill;
+    }
+}
+
+
+let users = [];
+function add() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+
+    let user = {
+        name: name,
+        email: email
+    }
+
+    users.push(user);
 }
